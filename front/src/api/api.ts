3a0d31c39141d64340e97bfd61bc285a7000fd67
Rafael <rafael@gregorini.com.br>
2024-03-16 23:01:57 -0300

    fix: fixing api url

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.API_URL || 'http://localhost:3000';
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
 export const fetchTransactions = async (filter: Record<string, string | undefined> | undefined) => { 
   try {
