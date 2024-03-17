8c4a1d228fcccd71d31d43daddbe331b8c0dfff8
Rafael <rafael@gregorini.com.br>
2024-03-17 09:31:37 -0300

    fix: adding env var api_url to populateDb script

--- api/populateDb.ts
+++ api/populateDb.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
-
+import * as dotenv from 'dotenv';
 async function createRandomTransaction() {
+  dotenv.config();
+
   const amount = (Math.random() * 50000).toFixed(2); // Valor aleatório entre 0 e 50000
   const type = Math.random() > 0.5 ? 'CREDIT' : 'DEBIT'; // Tipo aleatório
+  const apiUrl = process.env.API_URL || 'http://localhost:3000';
+  console.log(apiUrl);
 
   try {
-    await axios.post('http://localhost:3000/transactions', { amount, type });
+    await axios.post(`${apiUrl}/transactions`, { amount, type });
     console.log('Transação criada com sucesso:', { amount, type });
   } catch (error) {
     console.error('Erro ao criar transação:', error.response?.data);
@@ -18,6 +22,7 @@ async function main() {
     promises.push(createRandomTransaction());
   }
   await Promise.all(promises);
+  console.log('Transações criadas com sucesso');
 }
 
 main();
